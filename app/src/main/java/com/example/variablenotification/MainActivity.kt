0c6c452b84f2c1e24a3b2f5a59bfddde2af07116
Kei-no-suke <55536596+Kei-no-suke@users.noreply.github.com>
2023-08-24 12:54:04 +0900

    added notification permission request and organized buttons in MainActivity

--- app/src/main/java/com/example/variablenotification/MainActivity.kt
+++ app/src/main/java/com/example/variablenotification/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.variablenotification
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.variablenotification.ui.theme.VariableNotificationTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -31,8 +38,36 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject lateinit var timerOperator: TimerOperator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val requestPermissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestPermission(),
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Toast.makeText(
+                        this,
+                        "通知：許可済",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "通知：未許可",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
         setContent {
             VariableNotificationTheme {
                 var seconds = remember {
@@ -59,7 +94,7 @@ fun InputField(seconds: MutableState<String>, timerOperator: TimerOperator, cont
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
+            .padding(20.dp)
     ) {
         Row {
             Text(
@@ -75,27 +110,51 @@ fun InputField(seconds: MutableState<String>, timerOperator: TimerOperator, cont
                 modifier = Modifier.weight(3f)
             )
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text("通知")
         Row {
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { startNotifyForegroundService(context) },
+                modifier = Modifier.weight(2f)
+            ) {
+                Text("表示")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { stopNotifyForegroundService(context) },
+                modifier = Modifier.weight(2f)
+            ) {
+                Text("削除")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text("タイマー")
+        Row {
+            Spacer(modifier = Modifier.weight(1f))
             Button(
                 onClick = {
                     timerOperator.setTimer(seconds.value.toLongOrNull() ?: 0)
                     timerOperator.timerStart()
-                }
+                },
+                modifier = Modifier.weight(2f)
             ) {
-                Text("確定")
+                Text("開始")
             }
+            Spacer(modifier = Modifier.weight(1f))
             Button(
-                onClick = {
-                    startNotifyForegroundService(context)
-                }
+                onClick = { timerOperator.timerStop() },
+                modifier = Modifier.weight(2f)
             ) {
-                Text("通知表示")
+                Text("停止")
             }
+            Spacer(modifier = Modifier.weight(1f))
         }
-
     }
 }
 
+// for preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputField(seconds: MutableState<String>, modifier: Modifier = Modifier) {
@@ -104,7 +163,7 @@ fun InputField(seconds: MutableState<String>, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
+            .padding(20.dp)
     ) {
         Row {
             Text(
@@ -120,17 +179,43 @@ fun InputField(seconds: MutableState<String>, modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(3f)
             )
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text("通知")
         Row {
+            Spacer(modifier = Modifier.weight(1f))
             Button(
-                onClick = { }
+                onClick = { },
+                modifier = Modifier.weight(2f)
             ) {
-                Text("確定")
+                Text("表示")
             }
+            Spacer(modifier = Modifier.weight(1f))
             Button(
-                onClick = { }
+                onClick = { },
+                modifier = Modifier.weight(2f)
             ) {
-                Text("通知表示")
+                Text("削除")
             }
+            Spacer(modifier = Modifier.weight(1f))
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text("タイマー")
+        Row {
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { },
+                modifier = Modifier.weight(2f)
+            ) {
+                Text("開始")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { },
+                modifier = Modifier.weight(2f)
+            ) {
+                Text("停止")
+            }
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
